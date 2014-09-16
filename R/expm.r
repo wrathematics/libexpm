fdcca774a1ecd2705c05d17082b3543fb1934651
wrathematics <wrathematics@gmail.com>
2014-09-16 17:02:13 -0400

    R bindings

---
+++ R/expm.r
@@ -0,0 +1,18 @@
+# p is the order of the Pade' approximation. expokit uses 6. For us, 0<p<14
+expm <- function(x, p=6)
+{
+  if (nrow(x) != ncol(x))
+    stop("Matrix exponentiation is only defined for square matrices.")
+  
+  if (p < 1L || p > 13L)
+    stop("argument 'p' must be between 1 and 13.")
+   
+  if (!is.double(x))
+    storage.mode(x) <- "double"
+  
+  exp <- .Call("R_matexp", x, as.integer(p))
+  
+  return( exp )
+}
+
+
---
+++ R/test.r
@@ -0,0 +1,15 @@
+dyn.load("expm.so")
+source("expm.r")
+
+n <- 500
+x <- matrix(rnorm(n*n), n, n)
+
+t1 <- system.time(exp1 <- expm(x))[3]
+
+library(Matrix)
+t2 <- system.time(exp2 <- Matrix::expm(x))[3]
+
+t1
+t2
+
+all.equal(exp1, as.matrix(exp2), check.attributes=FALSE)
