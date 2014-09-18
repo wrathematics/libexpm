4593c58f52832208daccc287874ac280c2254524
wrathematics <wrathematics@gmail.com>
2014-09-18 19:19:13 -0400

    fixed copyright

--- R/expm_wrap.c
+++ R/expm_wrap.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013-2014 Drew Schmidt. All rights reserved.
+/* Copyright (C) 2014 Drew Schmidt. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
